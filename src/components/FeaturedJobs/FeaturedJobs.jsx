fbbeaba8a7a9c777949e5d23bd55dc6dab1b1b86
Rabbi1089 <rabbi.ext@gmail.com>
2024-03-30 02:03:14 +0600

    handle data length with slice and use-state

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -5,22 +5,31 @@ import Job from "../../job/Job";
 
 const FeaturedJobs = () => {
 
-    const [jobs , setJobs ] = useState([]);
+    const [jobs, setJobs] = useState([]);
+    const [dataLength, setDataLength] = useState(4)
 
-        useEffect(() => {
-            fetch('jobs.json')
+    useEffect(() => {
+        fetch('jobs.json')
             .then(res => res.json())
             .then(data => setJobs(data))
-        }, [])
+    }, [])
 
     return (
         <div>
             <div className=" grid grid-cols-2 gap-6 my-6">
                 {
-                    jobs.map((job , idx) => <Job key={idx} job={job}></Job>)
+                    jobs.slice(0, dataLength).map((job, idx) => <Job key={idx} job={job}></Job>)
                 }
-             
+
             </div>
+            <div className={dataLength === jobs.length && "hidden"}>
+                <button onClick={() => setDataLength(jobs.length)}
+                    className="btn btn-primary">Show all Jobs
+                </button>
+            </div>
+
+
+
         </div>
     );
 };
